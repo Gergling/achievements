ac3e35a6f397df4c93249b0edd3d2faad8c2f76a
Gergling <gregmichaeldavies@gmail.com>
2024-07-20 20:28:39 +0100

    feat: Removed default list styling and refactored.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,111 +1,11 @@
-import { useEffect, useState } from 'react';
-import { list } from '../utils/achievement-list';
-import { List } from './Dashboard.style';
-import { DashboardAchievement } from './DashboardAchievement';
-import { AchievementConfig } from '../types';
-import { isSince } from '../utils/is-since';
 import { DashboardRecent } from './DashboardRecent';
-
-type Group = {
-  label: string;
-  achievements: AchievementConfig[];
-};
-
-type GroupingMode = 'date' | 'rating';
-
-const dateGroups = {
-  '7 Days': (date: Date) => isSince(date, 7),
-  '30 Days': (date: Date) => isSince(date, 30),
-  '1 Year': (date: Date) => isSince(date, 365),
-  'Earlier': () => true,
-};
-
-const ratingLabels = [
-  { rating: 'gold', label: 'Gold' },
-  { rating: 'silver', label: 'Silver' },
-  { rating: 'bronze', label: 'Bronze' },
-];
-
-const getGroupingByDate = (achievementsOriginal: AchievementConfig[]): Group[] => {
-  let achievements = achievementsOriginal.map((achievement) => ({
-    achievement,
-    isGrouped: false,
-  }));
-  return Object.entries(dateGroups).map(([label, check]) => {
-    return {
-      label,
-      achievements: achievements.filter((achievement) => {
-        if (!achievement.isGrouped && check(achievement.achievement.date)) {
-          achievement.isGrouped = true;
-          return true;
-        }
-        return false;
-      }).map(({ achievement }) => achievement),
-    };
-  });
-};
-
-const getGroupingByRating = (achievements: AchievementConfig[]): Group[] =>
-  ratingLabels.map(({ rating, label }) => ({
-    label,
-    achievements: achievements.filter((achievement) => achievement.rating === rating),
-  }));
+import { GroupedAchievements } from './grouped/GroupedAchievements';
 
 export const Dashboard = () => {
-  const [grouping, setGrouping] = useState<Group[]>([]);
-  const [groupingMode, setGroupingMode] = useState<GroupingMode>('date');
-
-  const updateGrouping = () => {
-    if (groupingMode === 'date') {
-      setGrouping(getGroupingByDate(list));
-    } else {
-      setGrouping(getGroupingByRating(list));
-    }
-  };
-
-  const handleGroupingMode = (mode: GroupingMode) => () => {
-    setGroupingMode(mode);
-  };
-
-  useEffect(() => {
-    updateGrouping();
-  }, [groupingMode]);
-
   return (
     <>
       <DashboardRecent />
-      By
-      <button onClick={handleGroupingMode('date')}>Date</button>
-      |
-      <button onClick={handleGroupingMode('rating')}>Rating</button>
-      <ul>
-        {grouping
-          .filter(({ achievements }) => achievements.length > 0)
-          .map(({ label, achievements }) => (
-            <li key={label}>
-              {grouping.length > 1 ? label : ''}
-              <List>
-                {achievements.map(({
-                  name,
-                  pillar,
-                  interest,
-                  rating,
-                  date,
-                  description,
-                }) => <DashboardAchievement
-                  key={name}
-                  name={name}
-                  pillar={pillar}
-                  interest={interest}
-                  rating={rating}
-                  date={date}
-                  description={description}
-                />)}
-              </List>
-            </li>
-          )
-        )}
-      </ul>
+      <GroupedAchievements />
     </>
   );
 };
