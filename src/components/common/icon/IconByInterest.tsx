6520647a753c518c224d80290cb23312f5732b31
Gergling <gregmichaeldavies@gmail.com>
2024-09-01 12:27:35 +0100

    feat: Interest icons.
    (cherry picked from commit 3929f2b8fd2508084246132e2ae1af1e75bf2f86)

--- src/components/common/icon/IconByInterest.tsx
+++ src/components/common/icon/IconByInterest.tsx
@@ -1,6 +1,11 @@
 import CodeBrackets from '../../../assets/code-brackets.svg';
+import ComedyTragedyMasks from '../../../assets/comedy-tragedy-masks.svg';
 import Dumbbell from '../../../assets/dumbbell.svg';
+import Games from '../../../assets/games.svg';
 import HammerSpanner from '../../../assets/hammer-spanner.svg';
+import KickingStickMan from '../../../assets/kicking-stick-man.svg';
+import PaperPencilGear from '../../../assets/paper-pencil-gear.svg';
+import QuillScroll from '../../../assets/quill-scroll.svg';
 import { Unknown } from './Icon.style';
 import { IconProps } from "./types";
 
@@ -12,8 +17,18 @@ export const IconByInterest = ({
       return <CodeBrackets />;
     case 'crafts':
       return <HammerSpanner />;
+    case 'games':
+      return <Games />;
     case 'lifting':
       return <Dumbbell />;
+    case 'maintenance':
+      return <PaperPencilGear />;
+    case 'martialarts':
+      return <KickingStickMan />;
+    case 'performance':
+      return <ComedyTragedyMasks />;
+    case 'writing':
+      return <QuillScroll />;
   }
   return <Unknown>?</Unknown>;
 }
